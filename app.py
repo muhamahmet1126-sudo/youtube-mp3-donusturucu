f91cad111b5fb72d6a57ce91291ee809870a1dff
muhamahmet1126-sudo <muhamahmet1126@gmail.com>
2026-05-06 15:20:49 +0300

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
 import streamlit as st
+from audio_downloader import download_audio
+import os
 
 st.title("YouTube MP3 Dönüştürücü")
-st.write("Sistem şu an yükleniyor, lütfen bekleyin...")
 
-url = st.text_input("YouTube Video Linkini Buraya Yapıştırın:")
+url = st.text_input("YouTube Video Linkini Yapıştır:")
+
 if st.button("Dönüştür"):
-    st.info("İşlem başlatılıyor...")
+    if url:
+        with st.spinner("Müzik indiriliyor ve dönüştürülüyor... Lütfen bekleyin."):
+            try:
+                # İndirme işlemini başlat
+                file_path = download_audio(url)
+                
+                if os.path.exists(file_path):
+                    with open(file_path, "rb") as file:
+                        st.success("Dönüştürme Başarılı!")
+                        st.download_button(
+                            label="MP3 Dosyasını İndir",
+                            data=file,
+                            file_name=os.path.basename(file_path),
+                            mime="audio/mpeg"
+                        )
+                else:
+                    st.error("Dosya oluşturulamadı.")
+            except Exception as e:
+                st.error(f"Bir hata oluştu: {e}")
+    else:
+        st.warning("Lütfen bir URL girin.")
